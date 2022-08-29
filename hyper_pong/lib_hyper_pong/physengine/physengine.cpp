48c2f3f665b29751294a32c396f33fcc82ef4d52
Tri-Tachyon <vvokhom@gmail.com>
2022-08-29 20:09:23 +0300

    Applied ball radius to collisions

--- hyper_pong/lib_hyper_pong/physengine/physengine.cpp
+++ hyper_pong/lib_hyper_pong/physengine/physengine.cpp
@@ -31,9 +31,13 @@ float inline getCollisionCoef(float collX, float x1, float x2) {
   return (collX - x1) * 2 / (x2 - x1) - 1;
 }
 
+float inline signum(float num) {
+  return std::abs(num) / num;
+}
+
 // todo: revise formula
 float inline getReflectedPosition(float ballX, float ballDX, float wallX) {
-  return wallX - (std::abs(ballDX) / ballDX) * std::abs(ballDX - std::abs(wallX - ballX));
+  return wallX - signum(ballDX) * std::abs(ballDX - std::abs(wallX - ballX));
 }
 
 float inline getYOfIntersection(float vLineX, float lineX, float lineY, float vecX, float vecY) {
@@ -49,12 +53,16 @@ void PhysEngine::interactBall_VerticalRacket(AbstractActiveItem* ball, AbstractP
 
   const float ballDX = ball->getDX();
   const int ballRadius = ball->getSize();
+  //The closest point of the ball to the racket, or perp. to the racket if ball intersects it:
+  const float colPointX = ballX + signum( racketX - ballX) * std::min<float>(std::abs(racketX - ballX), ballRadius);
 
-  if (!isPointInBetween(racketX , ballX, ballX + ballDX)) return; // is ball close enough to interact
+  if (!isPointInBetween(racketX , colPointX, colPointX + ballDX)) return; // is ball close enough to interact
+
+
 
   const float ballY = ball->getY();
   const float ballDY = ball->getDY();
-  const float intersectionY = getYOfIntersection(racketX, ballX, ballY, ballDX, ballDY);
+  const float intersectionY = getYOfIntersection(racketX, colPointX, ballY, ballDX, ballDY);
 
   const float racketY1 = racket->getY();
   const float racketY2 = racket->getY() + racket->getSize();
@@ -73,11 +81,11 @@ void PhysEngine::interactBall_VerticalRacket(AbstractActiveItem* ball, AbstractP
 
   //moving the ball in a broken line:
 
-  ball->applyPosition(getReflectedPosition(ballX, ballDX, racketX), ballY + ballDY);
+  ball->applyPosition(getReflectedPosition(ballX, ballDX, racketX + signum(ballX - racketX) * ballRadius), ballY + ballDY);
   ball->skipNextMove();
 
   std::cout << "intersected!" << std::endl;
-  const float collisionCoef = getCollisionCoef(ballY, racketY1, racketY2);
+  const float collisionCoef = getCollisionCoef(intersectionY, racketY1, racketY2);
 
   float newAngle = maxReflectionAngle * collisionCoef;
 
@@ -94,15 +102,19 @@ void PhysEngine::interactBall_HorisontalWall(AbstractActiveItem* ball, AbstractP
   const float ballY = ball->getY();
   const float ballDY = ball->getDY();
   const int ballRadius = ball->getSize();
+  //The closest point of the ball to the wall, or perp. to the wall if ball intersects it:
 
-  if (!isPointInBetween(wallY , ballY, ballY + ballDY)) return; // is ball close enough to interact
+  const float colPointY = ballY + signum( wallY - ballY) * std::min<float>(std::abs(wallY - ballY), ballRadius);
+
+
+  if (!isPointInBetween(wallY , colPointY, colPointY + ballDY)) return; // is ball close enough to interact
 
   const float ballDX = ball->getDX();
 
 
   //moving the ball in a broken line:
   const float ballX = ball->getX();
-  ball->applyPosition(ballX + ballDX, getReflectedPosition(ballY, ballDY, wallY));
+  ball->applyPosition(ballX + ballDX, getReflectedPosition(ballY, ballDY, wallY + signum(ballX - wallY) * ballRadius));
   ball->skipNextMove();
 
   ball->applyVector(ballDX, -1 * ballDY);
